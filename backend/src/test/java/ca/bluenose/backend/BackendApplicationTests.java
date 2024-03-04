ddd5c02ebd407f2e440d099fabc1aff29743d313
JohnD <98172745+dangjohnD@users.noreply.github.com>
2024-03-03 22:11:30 -0500

    added backend

---
+++ backend/src/test/java/ca/bluenose/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bluenose.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
