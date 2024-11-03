fc423229a79383064f9b8557fe66a131c7a35ee3
mianmur <mianmur@sheridancollege.ca>
2024-11-03 14:23:24 -0500

    Added Cat bean and CatRepository (jparepository) to set up for CatService

---
+++ backend/src/main/java/ca/bluenose/backend/beans/Cat.java
@@ -0,0 +1,42 @@
+package ca.bluenose.backend.beans;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "cats")
+public class Cat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birthday", nullable = false)
+    //this specifies date only and not time
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+
+    @Column(name = "colour", nullable = false)
+    private String colour;
+
+    @Column(name = "description")
+    private String desc;
+
+    @Column(name = "is_disabled", nullable = false)
+    private boolean isDisabled;
+
+    @Column(name = "note")
+    private String note;
+
+}
